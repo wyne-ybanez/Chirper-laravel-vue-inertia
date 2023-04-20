4e935be42f8ef9de7a4af3a28d28e2540a610027
wyne-ybanez <wyne_01@icloud.com>
2023-04-20 16:48:19 +0100

    update: adds form template

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ChirpController extends Controller
 {
@@ -13,7 +14,9 @@ class ChirpController extends Controller
      */
     public function index(): Response
     {
-        return response('Hello World!');
+        return Inertia::render('Chirps/Index', [
+            //
+        ]);
     }
 
     /**
