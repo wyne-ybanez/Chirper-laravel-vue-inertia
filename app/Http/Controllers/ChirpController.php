db8cd1ac935bfbd1dbdce96e9b5ca5ac551eda79
wyne-ybanez <wyne_01@icloud.com>
2023-04-21 16:11:00 +0100

    feat: allows authors to edit their chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ValidateChirp;
 use App\Models\Chirp;
-use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -15,7 +15,7 @@ class ChirpController extends Controller
      * Display a listing of the resource.
      */
     public function index(): Response
-    {   
+    {
         return Inertia::render('Chirps/Index', [
             'chirps' => Chirp::with('user:id,name')->latest()->get(),
         ]);
@@ -32,12 +32,10 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
-    {
-        $validated = $request->validate([
-            'message' => 'required|string|max:255',
-        ]);
-
+    public function store(ValidateChirp $request): RedirectResponse
+    {   
+        // validated via 'ValidateChirp.php'
+        $validated = $request->validated();
         $request->user()->chirps()->create($validated);
 
         return redirect(route('chirps.index'));
@@ -62,9 +60,15 @@ class ChirpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(ValidateChirp $request, Chirp $chirp)
     {
-        //
+        $this->authorize('update', $chirp);
+
+        // validated via 'ValidateChirp.php'
+        $validated = $request->validated();
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
